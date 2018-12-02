af1687e073ec5049580b984acd1f166502175f39
Heerco_Grond <heercogrond@live.nl>
2018-12-02 21:40:42 +0100

    Testing purposes

--- Cha5ebot.py
+++ Cha5ebot.py
@@ -7,7 +7,7 @@ import os
 import configparser
 
 client = commands.Bot(command_prefix='>')
-extensions = ['modules.charactersheet','modules.statarray']
+extensions = ['modules.charactersheet','modules.statarray','modules.spells']
 currentPath = os.path.dirname(os.path.abspath(inspect.getfile(inspect.currentframe())))
 
 @client.event
