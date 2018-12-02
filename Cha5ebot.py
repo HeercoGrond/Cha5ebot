520241c2b04183ba3d91d7146e7a53c09d013c7b
Heerco_Grond <heercogrond@live.nl>
2018-12-02 19:17:40 +0100

    Fixed Cha5ebot to work with 1.0.0a some more and support cogs

--- Cha5ebot.py
+++ Cha5ebot.py
@@ -1,4 +1,5 @@
 import discord
+from discord.ext import commands
 import asyncio
 from random import randint
 import re
@@ -6,45 +7,66 @@ import inspect
 import os
 import configparser
 
+client = commands.Bot(command_prefix = ">")
 
 currentPath = os.path.dirname(os.path.abspath(inspect.getfile(inspect.currentframe())))
 
-class ChaseBot(discord.Client):
-    async def on_ready(self):
-        print('Logged in as')
-        print(self.user.name)
-        print(self.user.id)
-        print('------')
+@client.event
+async def on_ready():
+    print('Logged in as')
+    print(client.user.name)
+    print(client.user.id)
+    print('------')
 
+@client.event
+async def on_message(message):
+    if message.author.id != client.user.id:
+        channel = message.channel
+        if "!statarray" in message.content:
+            stats = []
+            for x in range(6):
+                currentStat = []
+                for y in range(4):
+                    currentStat.append(randint(1, 6))
 
-    async def on_message(self, message):
-        if message.author.id != client.user.id:
-            channel = message.channel
-            if "!statarray" in message.content:
-                stats = []
-                for x in range(6):
-                    currentStat = []
-                    for y in range(4):
-                        currentStat.append(randint(1, 6))
+                lowestDie = min(currentStat)
+                currentStat.remove(lowestDie)
+                stats.append(sum(currentStat))
 
-                    lowestDie = min(currentStat)
-                    currentStat.remove(lowestDie)
-                    stats.append(sum(currentStat))
+            statsString = ' '.join(str(e) for e in stats)
+            await channel.send("Current array for assignable stats is: " + statsString)
 
-                statsString = ' '.join(str(e) for e in stats)
-                await channel.send("Current array for assignable stats is: " + statsString)
+        if message.content.startswith("!roll"):
+            detailed = "detailed" in message.content
+            firstcheck = re.search(r'[0-9]+d[0-9]+[+][0-9]+', message.content)
+            if firstcheck: 
+                outcomeList = "{ "
+                matchString = str(firstcheck.group())
+                argsList = matchString.split('d')
+                splitArg = argsList[1].split('+')
+                diceAmount = int(argsList[0])
+                diceEyes = int(splitArg[0])
+                bonus = int(splitArg[1])
+                outcome = 0
+                for x in range(1, diceAmount + 1):
+                    randomNumber = randint(1, diceEyes)
+                    if detailed:
+                        outcomeList += str(randomNumber) + " , "
+                    outcome += randomNumber
 
-            if message.content.startswith("!roll"):
-                detailed = "detailed" in message.content
-                firstcheck = re.search(r'[0-9]+d[0-9]+[+][0-9]+', message.content)
-                if firstcheck: 
+                outcome += bonus
+                await channel.send("Rolled " + str(diceAmount) + " d" + str(diceEyes) + " with a bonus of " + str(bonus) + " for a total count of: " + str(outcome))
+                if detailed:
+                        await channel.send("Details on the rolls: " + outcomeList + " }")
+                
+            else:
+                filteredString = re.search(r'[0-9]+d[0-9]+', message.content)
+                if filteredString:
                     outcomeList = "{ "
-                    matchString = str(firstcheck.group())
-                    argsList = matchString.split('d')
-                    splitArg = argsList[1].split('+')
+                    matchedString = str(filteredString.group())
+                    argsList = matchedString.split('d')
                     diceAmount = int(argsList[0])
-                    diceEyes = int(splitArg[0])
-                    bonus = int(splitArg[1])
+                    diceEyes = int(argsList[1])
                     outcome = 0
                     for x in range(1, diceAmount + 1):
                         randomNumber = randint(1, diceEyes)
@@ -52,59 +74,37 @@ class ChaseBot(discord.Client):
                             outcomeList += str(randomNumber) + " , "
                         outcome += randomNumber
 
-                    outcome += bonus
-                    await channel.send("Rolled " + str(diceAmount) + " d" + str(diceEyes) + " with a bonus of " + str(bonus) + " for a total count of: " + str(outcome))
+                    await channel.send("Rolled " + str(diceAmount) + " d" + str(diceEyes) + " for a total count of: " + str(outcome))
                     if detailed:
-                            await channel.send("Details on the rolls: " + outcomeList + " }")
-                    
+                        await channel.send("Details on the rolls: " + outcomeList + " }")
                 else:
-                    filteredString = re.search(r'[0-9]+d[0-9]+', message.content)
-                    if filteredString:
-                        outcomeList = "{ "
-                        matchedString = str(filteredString.group())
+                    secondaryFilter = re.search(r'd[0-9]+', message.content)
+                    if secondaryFilter:
+                        matchedString = str(secondaryFilter.group())
                         argsList = matchedString.split('d')
-                        diceAmount = int(argsList[0])
                         diceEyes = int(argsList[1])
-                        outcome = 0
-                        for x in range(1, diceAmount + 1):
-                            randomNumber = randint(1, diceEyes)
-                            if detailed:
-                                outcomeList += str(randomNumber) + " , "
-                            outcome += randomNumber
-
-                        await channel.send("Rolled " + str(diceAmount) + " d" + str(diceEyes) + " for a total count of: " + str(outcome))
-                        if detailed:
-                            await channel.send("Details on the rolls: " + outcomeList + " }")
+                        outcome = randint(1, diceEyes)
+                        await channel.send("Rolled a d" + str(diceEyes) + " for: " + str(outcome))
                     else:
-                        secondaryFilter = re.search(r'd[0-9]+', message.content)
-                        if secondaryFilter:
-                            matchedString = str(secondaryFilter.group())
-                            argsList = matchedString.split('d')
-                            diceEyes = int(argsList[1])
-                            outcome = randint(1, diceEyes)
-                            await channel.send("Rolled a d" + str(diceEyes) + " for: " + str(outcome))
-                        else:
-                            await channel.send("That is not a valid die. Proper formatting is `!roll {x]d{y}` or `!roll d{x}`")
+                        await channel.send("That is not a valid die. Proper formatting is `!roll {x]d{y}` or `!roll d{x}`")
 
-            if message.content.startswith("!charsheet"):
-                if message.guild != None:
-                    currentGuildPath = currentPath + "/guilds/" + str(message.guild.id)
-                    print(currentGuildPath)
+        if message.content.startswith("!charsheet"):
+            if message.guild != None:
+                currentGuildPath = currentPath + "/guilds/" + str(message.guild.id)
+                print(currentGuildPath)
 
-                    if not os.path.exists(currentGuildPath):
-                        os.makedirs(currentGuildPath)
+                if not os.path.exists(currentGuildPath):
+                    os.makedirs(currentGuildPath)
 
-                    currentUserPath = currentGuildPath + "/user/" + str(message.author.id)
-                    if not os.path.exists(currentUserPath):
-                        os.makedirs(currentUserPath)
-                        print("Created charsheet folder for user " + str(message.author.id))
-                
+                currentUserPath = currentGuildPath + "/user/" + str(message.author.id)
+                if not os.path.exists(currentUserPath):
+                    os.makedirs(currentUserPath)
+                    print("Created charsheet folder for user " + str(message.author.id))
+            
 
 config = configparser.ConfigParser()
 config.read(currentPath + "/config/bot.ini")
 token = config['DEFAULT']['token']
-
-client = ChaseBot()
 
 if token != "":
     client.run(token)
--- modules/charactersheet.py
+++ modules/charactersheet.py
@@ -1,53 +1,12 @@
-classes = []
+import discord
+from discord.ext import commands
 
 class CharacterSheet:
-    def __init__(self):
-        # User Attributes
-        self.server = "3453453453"
-        self.userid = "3456356345"
-        
-        # Dnd Attributes
-        self.stats = {
-            "Strength": [0, 0],
-            "Dexterity": [0, 0],
-            "Constitution": [0, 0],
-            "Intelligence": [0, 0],
-            "Wisdom": [0, 0],
-            "Charisma": [0, 0]
-        }
+    def __init__(self, client):
+        self.client = client;
 
-        self.skills = {
-            "Athletics": [0, 0],
-            "Acrobatics": [0, 0],
-            "Sleight of Hand": [0, 0],
-            "Stealth": [0, 0],
-            "Arcana": [0, 0],
-            "History": [0, 0],
-            "Investigation": [0, 0],
-            "Nature": [0, 0],
-            "Religion": [0, 0],
-            "Animal Handling": [0, 0],
-            "Insight": [0, 0],
-            "Medicine": [0, 0],
-            "Perception": [0, 0],
-            "Survival": [0, 0],
-            "Deception": [0, 0],
-            "Intimidation": [0, 0],
-            "Performance": [0, 0],
-            "Persuasion": [0, 0]
-        }
+    async def on_ready(self):
+        print("loaded cog")
 
-        self.equipment = { }
-
-
-    def encode():
-        print("encoding")
-
-    def decode(object):
-        print("decoding")
-
-    def displaySheet():
-        print("display the sheet and stuff")
-        
-
-x = CharacterSheet()
+def setup(client):
+    client.add_cog(CharacterSheet(client))
