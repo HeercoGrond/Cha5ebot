d6517e4a587d94d2e96b2926f3449dc914955749
HeercoGrond <heercogrond@live.nl>
2018-12-05 00:04:49 +0100

    charsheet command update to support creating and retrieving of json

--- modules/charactersheet.py
+++ modules/charactersheet.py
@@ -2,10 +2,11 @@ import discord
 from discord.ext import commands
 import os 
 import inspect
+import json
 
 currentPath = os.path.dirname(os.path.abspath(inspect.getfile(inspect.currentframe())))[:-8] 
 
-class CharacterSheet:
+class CharacterSheetCog:
     def __init__(self, client):
         self.client = client
 
@@ -15,16 +16,28 @@ class CharacterSheet:
     @commands.command()
     async def charsheet(self, ctx):
         if ctx.guild != None:
-            currentGuildPath = currentPath + "/guilds/" + str(ctx.guild.id)
-            print(currentGuildPath)
-
-            if not os.path.exists(currentGuildPath):
-                os.makedirs(currentGuildPath)
+            currentGuildPath =  "/guilds/" + str(ctx.guild.id)
+            if not os.path.exists(currentPath + currentGuildPath):
+                os.makedirs(currentPath + currentGuildPath)
 
             currentUserPath = currentGuildPath + "/user/" + str(ctx.author.id)
-            if not os.path.exists(currentUserPath):
-                os.makedirs(currentUserPath)
+            if not os.path.exists(currentPath + currentUserPath):
+                os.makedirs(currentPath + currentUserPath)
                 print("Created charsheet folder for user " + str(ctx.author.id))
 
+                with open("." + currentUserPath + "/charactersheet.json", 'w') as fp:
+                    data = self.make_charsheet()
+                    data["key"] = 'value'
+                    json.dump(data, fp)
+             
+            with open("." + currentUserPath + "/charactersheet.json") as file:
+                data = json.load(file)
+                print(data)
+
+                await ctx.send(data["key"])
+
+    def make_charsheet(self):
+        return {}
+
 def setup(client):
-    client.add_cog(CharacterSheet(client))
+    client.add_cog(CharacterSheetCog(client))
