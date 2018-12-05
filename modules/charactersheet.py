cbe9618c49d9a9d271d5ba030ac598599267a5be
HeercoGrond <heercogrond@live.nl>
2018-12-05 23:26:32 +0100

    Base charactersheet creation

--- modules/charactersheet.py
+++ modules/charactersheet.py
@@ -1,5 +1,6 @@
 import discord
 from discord.ext import commands
+from enum import Enum
 import os 
 import inspect
 import json
@@ -14,30 +15,84 @@ class CharacterSheetCog:
         print("loaded cog")
 
     @commands.command()
-    async def charsheet(self, ctx):
+    async def charsheet(self, ctx, argument):
         if ctx.guild != None:
             currentGuildPath =  "/guilds/" + str(ctx.guild.id)
-            if not os.path.exists(currentPath + currentGuildPath):
-                os.makedirs(currentPath + currentGuildPath)
+            if not os.path.exists("." + currentGuildPath):
+                os.makedirs("." + currentGuildPath)
+                print("This path doesn't exist")
 
             currentUserPath = currentGuildPath + "/user/" + str(ctx.author.id)
-            if not os.path.exists(currentPath + currentUserPath):
-                os.makedirs(currentPath + currentUserPath)
+            if not os.path.exists("." + currentUserPath):
+                os.makedirs("." + currentUserPath)
                 print("Created charsheet folder for user " + str(ctx.author.id))
 
-                with open("." + currentUserPath + "/charactersheet.json", 'w') as fp:
-                    data = self.make_charsheet()
-                    data["key"] = 'value'
+            if not os.path.exists("." + currentUserPath + "/" + argument + ".json"):
+                with open("." + currentUserPath + "/" + argument + ".json", 'w') as fp:
+                    data = self.make_charsheet(argument, ctx.guild.id, ctx.author.id)
                     json.dump(data, fp)
+                    await ctx.send("Found charactersheet with the name: " + data["name"])
              
-            with open("." + currentUserPath + "/charactersheet.json") as file:
-                data = json.load(file)
-                print(data)
+            # with open("." + currentUserPath + "/" + argument + ".json") as file:
+            #     data = json.load(file)
+            #     await ctx.send("Found charactersheet with the name: " + data["name"])
 
-                await ctx.send(data["key"])
+    def make_charsheet(self, sheetname, guild_id, user_id):
+        charactersheet = {}
 
-    def make_charsheet(self):
-        return {}
+        charactersheet["name"] = sheetname
+        charactersheet["guild_id"] = guild_id
+        charactersheet["user_id"] = user_id
+
+        charactersheet["class"] = "undefined"
+        charactersheet["level"] = 1
+
+        charactersheet["attributes"] = {
+            "strength": 0,
+            "dexterity": 0,
+            "constitution": 0,
+            "intelligence": 0,
+            "wisdom": 0,
+            "charisma": 0
+        }
+
+        charactersheet["saving_throws"] = {
+            "strength": 0,
+            "dexterity": 0,
+            "constitution": 0,
+            "intelligence": 0,
+            "wisdom": 0,
+            "charisma": 0
+        }
+
+        charactersheet["skills"] = {
+            "athletics": 0,
+            "acrobatics": 0,
+            "sleight_of_hand": 0,
+            "stealth": 0,
+            "arcana": 0,
+            "history": 0,
+            "investigation": 0,
+            "nature": 0,
+            "religion": 0,
+            "animal_handling": 0,
+            "insight": 0,
+            "medicine": 0,
+            "perception": 0,
+            "survival": 0,
+            "deception": 0,
+            "intimidation": 0,
+            "performance": 0,
+            "persuasion": 0
+        } 
+
+        charactersheet["equipment"] = {}
+        charactersheet["actions"] = {}
+        charactersheet["spellbook"] = {}
+
+        return charactersheet
+
+
 
 def setup(client):
     client.add_cog(CharacterSheetCog(client))
