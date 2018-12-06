521912b3ae1b9f977ddfe267e862a96c2306e778
Puck van Zutphen <rachellepuck@Pucks-MBP.fritz.box>
2018-12-06 13:17:04 +0100

    Added spell and incapable of making roll work

--- modules/spells.py
+++ modules/spells.py
@@ -1,6 +1,9 @@
 from discord.ext import commands
 from random import randint
 import json
+import sys
+sys.path.append('./modules/libraries')
+import roll
 
 class Spells:
     def __init__(self, client):
@@ -18,6 +21,10 @@ class Spells:
     @commands.command()
     async def cast(self, ctx, *, arg):
         await ctx.send("Casting " + arg)
+        with open('./modules/libraries/spell_library.json') as f:
+            spell = json.load(f)
+
+        # await ctx.send(">roll " + spell[arg]["damage"])
         
 def setup(client):
     client.add_cog(Spells(client))
