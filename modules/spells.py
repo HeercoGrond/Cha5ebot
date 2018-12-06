7648348cb3d4acce7c1e4210f1a6b096ca9c6dbd
Puck van Zutphen <rachellepuck@Pucks-MBP.fritz.box>
2018-12-06 22:12:55 +0100

    Added more complete spell json file and fixed the cog to work with it

--- modules/spells.py
+++ modules/spells.py
@@ -12,19 +12,21 @@ class Spells:
 
     @commands.command()
     async def spell(self, ctx, *, arg):
-        with open('./modules/libraries/spell_library.json') as f:
+        with open('./modules/libraries/spells.json') as f:
             spell_data = json.load(f)
         await ctx.send(spell_data[arg])
 
     @commands.command()
     async def cast(self, ctx, *, arg):
         await ctx.send("Casting " + arg)
-        with open('./modules/libraries/spell_library.json') as f:
-            spell = json.load(f)
+        with open('./modules/libraries/spells.json') as f:
+            spells = json.load(f)
 
+            for spell in spells:
+                if spell["name"].lower() == arg.lower():
+                    dmg = spell["damage"]
+                    message = roll_dice(dmg)
+                    await ctx.send(message)
 
-            message = roll_dice(spell[arg]["damage"])
-            await ctx.send(message)
-        
 def setup(client):
     client.add_cog(Spells(client))
