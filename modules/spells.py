826c6764e96abf8b63b96c0fa6a56b40fe52c698
Heerco_Grond <heercogrond@live.nl>
2018-12-11 12:11:46 +0100

    Start with embedding spells

--- modules/spells.py
+++ modules/spells.py
@@ -1,4 +1,5 @@
 from discord.ext import commands
+import discord
 from random import randint
 import json
 from modules.libraries.roll import roll_dice
@@ -14,7 +15,18 @@ class Spells:
     async def spell(self, ctx, *, arg):
         with open('./modules/libraries/spells.json') as f:
             spell_data = json.load(f)
-            await ctx.send(spell_data[arg])
+            for spell in spell_data:
+                if spell["name"].lower() == arg.lower():
+                    spell_description = "Level " + spell["level"] + " " + spell["school"] + " Spell."
+                    embed_spell = discord.Embed(title=spell["name"], description=spell_description)
+
+                    embed_spell.add_field(name="Duration", value=spell["duration"])
+
+                    embed_spell.add_field(name="Components", value=spell["components"]["raw"])
+
+                    embed_spell.add_field(name="Description", value=spell["description"])
+
+                    await ctx.send(embed=embed_spell)
 
     @commands.command()
     async def cast(self, ctx, *, arg):
