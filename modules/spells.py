79d07171b73dcb588cda1a1da8b954c8aa17b552
Puck van Zutphen <rpvzutphen@gmail.com>
2018-12-15 22:03:16 +0100

    Required DM role to execute spell command

--- modules/spells.py
+++ modules/spells.py
@@ -13,28 +13,31 @@ class Spells:
 
     @commands.command()
     async def spell(self, ctx, *, arg):
-        with open('./modules/libraries/spells.json') as f:
-            spell_data = json.load(f)
-            for spell in spell_data:
-                if spell["name"].lower() == arg.lower():
-                    spell_description = "Level " + spell["level"] + " " + spell["school"] + " spell."
-                    embed_spell = discord.Embed(title=spell["name"], description=spell_description)
+        if discord.utils.get(ctx.guild.roles, name=self.client.dm_role) in ctx.message.author.roles:
+            with open('./modules/libraries/spells.json') as f:
+                spell_data = json.load(f)
+                for spell in spell_data:
+                    if spell["name"].lower() == arg.lower():
+                        spell_description = "Level " + spell["level"] + " " + spell["school"] + " spell."
+                        embed_spell = discord.Embed(title=spell["name"], description=spell_description)
 
-                    embed_spell.add_field(name="Duration", value=spell["duration"])
+                        embed_spell.add_field(name="Duration", value=spell["duration"])
 
-                    embed_spell.add_field(name="Casting Time", value=spell["casting_time"])
+                        embed_spell.add_field(name="Casting Time", value=spell["casting_time"])
 
-                    embed_spell.add_field(name="Components", value=spell["components"]["raw"])
+                        embed_spell.add_field(name="Components", value=spell["components"]["raw"])
 
-                    embed_spell.add_field(name="Description", value=spell["description"])
+                        embed_spell.add_field(name="Description", value=spell["description"])
 
-                    if "higher_levels" in spell:
-                        embed_spell.add_field(name="Higher Levels", value=spell["higher_levels"])
+                        if "higher_levels" in spell:
+                            embed_spell.add_field(name="Higher Levels", value=spell["higher_levels"])
 
-                    if spell["ritual"] == True:
-                        embed_spell.add_field(name="Ritual", value="Yes")
+                        if spell["ritual"] == True:
+                            embed_spell.add_field(name="Ritual", value="Yes")
 
-                    await ctx.send(embed=embed_spell)
+                        await ctx.send(embed=embed_spell)
+        else:
+            await ctx.send("You don't have permission to use that command.")
 
     @commands.command()
     async def cast(self, ctx, *, arg):
