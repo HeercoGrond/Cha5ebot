3dca606e13c46075f7c36c0b270e689f57211b58
Puck van Zutphen <rpvzutphen@gmail.com>
2019-01-09 11:22:06 +0100

    Formatting improvements

--- modules/custom.py
+++ modules/custom.py
@@ -18,19 +18,19 @@ class Custom:
     async def sneakattack(self, ctx):
         diceamount = str(randint(3, 100)) + "d6"
         totaldmg = roll_dice(diceamount)
-        await ctx.send("SNEAK ATTACK!\nEthearia rollt " + diceamount + " voor " + str(totaldmg[0]) + " damage.")
+        await ctx.send("SNEAK ATTACK!\nEthearia rollt %s voor %s damage." % (diceamount, totaldmg[0]))
 
     @commands.command()
     async def fireball(self, ctx):
         enemies = randint(3, 65)
         leach = 12
         health = enemies * leach
-        await ctx.send("FIREBALL!\nFreyja gooit een fireball op " + str(enemies) + " enemies en krijgt " + str(health) + " temporary hitpoints.")
+        await ctx.send("FIREBALL!\nFreyja gooit een fireball op %s enemies en krijgt %s temporary hitpoints." % (enemies, health))
 
     @commands.command()
     async def disappoint(self, ctx):
         disappointment = roll_dice("d20")
-        await ctx.send(ctx.author.name + " is " + str(disappointment[0]) + " disappoint.\nShame on you, shame on your mother, shame on your cow.")
+        await ctx.send(ctx.author.name + " is %s disappoint.\nShame on you, shame on your mother, shame on your cow." % (disappointment[0]))
 
     @commands.command()
     async def narcian(self, ctx):
@@ -39,7 +39,7 @@ class Custom:
     @commands.command()
     async def throw(self, ctx, arg):
         distance = randint(1, 150)
-        await ctx.send(ctx.author.name + " throws " + arg + " " + str(distance) + " feet away.\nYEEEEEEEEEEEEET!")
+        await ctx.send(ctx.author.name + " throws %s %s feet away.\nYEEEEEEEEEEEEET!" % (arg, distance))
 
 def setup(client):
     client.add_cog(Custom(client))
